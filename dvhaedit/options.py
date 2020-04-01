4b6412d8405b8c4e3e96ff6f5df00c26f96c943a
Dan Cutright <dan.cutright@gmail.com>
2020-03-31 20:56:31 -0500

    shebang, opening comments

--- dvhaedit/options.py
+++ dvhaedit/options.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# options.py
+"""
+Classes containing user preferences
+"""
+# Copyright (c) 2020 Dan Cutright
+# This file is part of DVH Analytics, released under a BSD license.
+#    See the file LICENSE included with this distribution, also
+#    available at https://github.com/cutright/DVHA-DICOM-Editor
+
 from dvhaedit.dicom_editor import get_uid_prefixes
 
 
